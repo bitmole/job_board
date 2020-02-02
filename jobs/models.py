2f7fdf8de9d3f4089140ab186a0ab6be94520c1c
Robert Hanacek <robert@idea-loop.com>
2020-02-01 18:24:16 -0700

    Initial app commit.

---
+++ jobs/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class JobOffer(models.Model):
+
+    """JobOffer model"""
+
+    company_name = models.CharField(max_length=60)
+    company_email = models.EmailField()
+    job_title = models.CharField(max_length=60)
+    job_description = models.TextField(blank=True)
+    salary = models.DecimalField()
+    city = models.CharField(max_length=60)
+    state = models.CharField(max_length=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
