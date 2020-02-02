8db9ed4c8d7c14260859ebdf4ca273e024a28c6c
Robert Hanacek <robert@idea-loop.com>
2020-02-01 22:04:20 -0700

    Add serializer

--- jobs/models.py
+++ jobs/models.py
@@ -10,8 +10,11 @@ class JobOffer(models.Model):
     company_email = models.EmailField()
     job_title = models.CharField(max_length=60)
     job_description = models.TextField(blank=True)
-    salary = models.DecimalField()
+    salary = models.IntegerField()
     city = models.CharField(max_length=60)
     state = models.CharField(max_length=2)
     created_at = models.DateTimeField(auto_now_add=True)
     available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{ self.job_title } at { self.company_name }'
